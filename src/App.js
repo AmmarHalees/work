1189fa6c9df3d5943a61535b6f2fcad04042439a
Ammar Halees <32368546+AmmarHalees@users.noreply.github.com>
2021-03-04 13:25:11 +0200

    clearing

--- src/App.js
+++ src/App.js
@@ -1,138 +1,14 @@
 import React from 'react';
-import './App.css';
-import Upper from './Components/Upper';
-import Mid from './Components/Mid';
-import Lower from './Components/Lower';
-import SpeedDialTooltipOpen from './ExternalComponents/SpeedDialTooltipOpen';
-import MetaTags from 'react-meta-tags';
+import './App.css'
 
+const App = () => {
+  return (
+    <div className="prompt_container" >
+      <p className="prompt">Moved  <a title="new website" rel="noreferrer noopener" href="https://ammarhalees.com/">here</a></p>
 
+    </div>
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { 
-
-      check1:false,
-      test:false,
-      containerStyle:
-      {
-        backgroundColor:"white"
-
-      }
-
-      ,
-
-      darkTheme:{
-
-        backgroundColor:"black",
-        background:"black",
-        color:"white",
-
-      }
-
-     }
-
-     this.handleMe=this.handleMe.bind(this)
-
-  }
-
-  handleMe(event){
-
-    const {name,checked}= event.target
-  
-    this.setState({
-  
-     
-      [name]:checked
-
-
-    },()=>console.log(`${[name]} +${[checked]}`))
-
-    if(!checked){
-
-      this.setState({
-  
-
-        
-        containerStyle: {
-          backgroundColor:"white"
-  
-        }
-    
-    
-      })
-    
-      
-
-    }else{
-
-
-      this.setState({
-  
-
-        
-        containerStyle: {
-          backgroundColor:"#020202"
-  
-        }
-    
-    
-      })
-    }
-
-
-
-    // this.setState({
-  
-    //   [name]:checked
-  
-  
-    // },()=>console.log(`${[name]} +${[checked]}`))
-  
-    
-  
-    }
-
-
-
-  render() { 
-    return ( 
-
-
-
-      <div className="container" style={this.state.containerStyle}>
-      {/* <div id="bg-bg"></div>
-                <div id="bg-bg2"></div>
-      <div id="bg-bg3"></div>*/}
-    
-    
-    <MetaTags>
-            <title>Ammar Halees | Work</title>
-            <meta name="description" content="Some description." />
-            {/* <meta property="og:title" content="MyApp" /> */}
-            {/* <meta property="og:image" content="path/to/image.jpg" /> */}
-          </MetaTags>
-    
-    
-    <Upper handleMe={this.handleMe}  check1={this.state.check1} darkTheme={this.state.darkTheme}/>
-    
- {/* {   <Mid check1={this.state.check1}/>} */}
-
-    
-    
-    <Lower check1={this.state.check1} />
-    
-    
-    <SpeedDialTooltipOpen check1={this.state.check1} darkTheme={this.state.darkTheme}/>
-    
-    
-    
-        </div> 
-
-     );
-  }
+  );
 }
- 
-export default App;
 
+export default App;
